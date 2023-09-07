107ca3a43e3287976d8b9e1e04ecba061a9d6661
tomrni <90772332+gitoror@users.noreply.github.com>
2023-09-07 19:53:37 +0200

    a

--- pages/test.js
+++ pages/test.js
@@ -1,13 +1,10 @@
 export default async function Test() {
-  const response = await fetch(
-    "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
-  );
-  console.log(response);
-  const data = await response.json();
-  console.log(data);
+  const day = new Date();
+  console.log(day);
   return (
     <div>
       <h1>Test</h1>
+      <p>{day}</p>
     </div>
   );
 }
